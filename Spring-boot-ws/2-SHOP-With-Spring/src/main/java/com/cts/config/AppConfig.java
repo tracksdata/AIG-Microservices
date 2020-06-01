bfa07e7fba7d262eda683c3e6038a861883b7ced
ctsjava <ctsgit@gmail.com>
2020-06-01 16:12:56 +0530

    spring basics

--- Spring-boot-ws/2-SHOP-With-Spring/src/main/java/com/cts/config/AppConfig.java
+++ Spring-boot-ws/2-SHOP-With-Spring/src/main/java/com/cts/config/AppConfig.java
@@ -1,10 +1,23 @@
 package com.cts.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource(value = {"application.properties"})
 @ComponentScan("com.pricematrix,com.bill")
 public class AppConfig {
+
+	@Value("${name}")
+	private String empName;
+
+	@Bean
+	public void test() {
+		System.out.println("====> Name: " + empName);
+
+	}
 
 }
